c4474c4eab9a76846e69d59c968465743d70941f
Soumodip Das <soumodipddas2005@gmail.com>
2024-09-09 16:55:46 +0000

    learning

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,18 @@
-
+import Chai from "./chai"
 
 function App() {
   return (
-    <h1>Hellow World | With vite</h1>
+    // <div>
+    //   <Chai/>
+    //   <h1>chai aur react</h1>
+    //   <p>test para</p>
+    // </div>
+    // or we can do this V (called as fragment)
+    <>
+      <Chai/>
+      <h1>chai aur react</h1>
+      <p>test para</p>
+    </>
   )
 }
 
