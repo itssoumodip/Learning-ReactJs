f504a70f9435e216b65c8876c6ebdcd7edbfe607
Soumodip Das <soumodipddas2005@gmail.com>
2024-09-11 14:28:47 +0000

    learning...

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -5,24 +5,26 @@ import './App.css'
 
 function App() {
 
-  const [counter, setCounter] = useState(15) //default value 
+  let [counter, setCounter] = useState(15) //default value 
 
   //let counter = 15
 
   const addValue = () => {
-    counter = counter + 1;
-    setCounter(counter)
-    console.log(counter)
+    // counter = counter + 1;
+    setCounter(counter+1)
+    // console.log(counter)
   }
 
+  const removeValue = () => {
+    setCounter(counter-1)
+  }
   return (
     <>
       <h1>Soumodip is my Name</h1>
       <h2>Counter Value: {counter}</h2>
 
       <button onClick={addValue}>Add Value</button>
-      <br />
-      <button>Remove Value</button>
+      <button onClick={removeValue}>Remove Value</button>
     </>
   )
 }
