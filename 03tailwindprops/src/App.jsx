2fc1bdd4ed4ae32b7971fc774708201a318f1e4b
Soumodip Das <soumodipddas2005@gmail.com>
2024-09-16 07:29:25 +0000

    learning tailwind...

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,13 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
+
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj = {
+    userName: "Soumodip",
+    age: 19
+  }
+  let newArr = [1,2,3]
 
   return (
     <>
-    <h2 className='bg-gree'>Tailwind Test</h2>
+      <h2 className='bg-green-400  text-black p-4 rounded-xl mb-4'>Tailwind Test</h2>
+      {/* <Card channel = "Adda & Knowledge" someObj={newArr}/> */}
+      <Card username = "Adda & Knowledge" btnText="Click Me"/>
+      <Card username = "Soumodip"/>
     </>
   )
 }
---
+++ 03tailwindprops/src/components/Card.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Card({username,btnText="VISIT ME"}) {
+
+  // console.log("props", props)
+  console.log(username)
+
+  return (
+    <div className="relative h-[400px] w-[300px] rounded-md">
+      <img
+        src="https://img.freepik.com/free-photo/full-shot-ninja-wearing-equipment_23-2150960979.jpg?t=st=1726467879~exp=1726471479~hmac=8e74f382eb2040df389085cacc2be333ea25cf9e14b377895b783b25ef6e7b21&w=1060&amp;fit=crop&amp;w=800&amp;q=60"
+        alt="AirMax Pro"
+        className="z-0 h-full w-full rounded-md object-cover"
+      /> 
+      <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+      <div className="absolute bottom-4 left-4 text-left">
+        <h1 className="text-lg font-semibold text-white">{username}</h1>
+        <p className="mt-2 text-sm text-gray-300">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+          debitis?
+        </p>
+        <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+          {btnText} →
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Card
