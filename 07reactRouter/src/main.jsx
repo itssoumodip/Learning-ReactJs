5cc0f49b65c25c378904e40cca9fb96dcb3644c8
Soumodip Das <soumodipddas2005@gmail.com>
2024-11-05 10:27:15 +0000

    learning

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -3,8 +3,32 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.jsx'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
 
-const router = 
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children: [
+      {
+        path: "",
+        element: <Home/>
+      },
+      {
+        path: "about",
+        element: <About/>
+      },
+      {
+        path: "contact",
+        element: <Contact/>
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
