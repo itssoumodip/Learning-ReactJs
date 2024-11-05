cbba0ba1bd76aff7fdec0d2925588565472b042f
Soumodip Das <soumodipddas2005@gmail.com>
2024-11-05 19:41:40 +0000

    7th project done.

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useLoaderData } from 'react-router-dom'
 
 function Github() {
+  const data = useLoaderData()
     // const [data, setData] = useState([])
     // useEffect(() => {
     //   fetch('https://api.github.com/users/itssoumodip')
