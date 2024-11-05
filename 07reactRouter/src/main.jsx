18770aab60a430338447c3e784b1bcf611766fea
Soumodip Das <soumodipddas2005@gmail.com>
2024-11-05 09:41:53 +0000

    learning...

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
