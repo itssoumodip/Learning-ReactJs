154df12ec8d5d7ed2d32d31a06153bbc251ea44e
Soumodip Das <soumodipddas2005@gmail.com>
2024-11-05 06:17:57 +0000

    learning

--- 06CurrencyConverter/src/hooks/useCurrencyInfo.js
+++ 06CurrencyConverter/src/hooks/useCurrencyInfo.js
@@ -1,19 +1,18 @@
 // API :  https://api.exchangerate-api.com/v4/latest/USD
 
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react"
 
 
-function useCurrencyInfo (currency){
+function useCurrencyInfo(currency){
     const [data, setData] = useState({})
-    useEffect(()=> {
-        fetch(`https://api.exchangerate-api.com/v4/latest/${currency}`)
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
         .then((res) => res.json())
         .then((res) => setData(res[currency]))
-        console.log(data)
-    }, [currency])   
-
-    console.log(data)
+        console.log(data);
+    }, [currency])
+    console.log(data);
     return data
 }
 
-export default useCurrencyInfo 
+export default useCurrencyInfo;
