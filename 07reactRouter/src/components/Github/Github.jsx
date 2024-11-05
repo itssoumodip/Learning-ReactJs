ebb683763372aeb689a8a4bd5da3baf6edde8396
Soumodip Das <soumodipddas2005@gmail.com>
2024-11-05 19:34:23 +0000

    learning learning

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,20 +1,27 @@
 import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 function Github() {
-    const [data, setData] = useState([])
-    useEffect(() => {
-      fetch('https://api.github.com/users/itssoumodip')
-      .then(response => response.json())
-      .then(data=> {
-        console.log(data);
-        setData(data)
-      })
-    }, [])
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //   fetch('https://api.github.com/users/itssoumodip')
+    //   .then(response => response.json())
+    //   .then(data=> {
+    //     console.log(data);
+    //     setData(data)
+    //   })
+    // }, [])
     
   return (
-    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github Followers: {data.followers}
+    <div className='text-center m-4 bg-black text-white p-4 text-3xl'>Github Followers: {data.followers}
+    <img src={data.avatar_url} alt="Git picture" width={300} />
     </div>
   )
 }
 
 export default Github
+
+export const githubInfoLoader = async() => {
+  const response = await fetch('https://api.github.com/users/itssoumodip')
+  return response.json()
+}
