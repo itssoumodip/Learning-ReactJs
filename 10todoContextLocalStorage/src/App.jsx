371f88d85d305b987305e67fd8c946ae6c36e240
Soumodip Das <soumodipddas2005@gmail.com>
2024-11-30 07:20:05 +0000

    learning....

--- 10todoContextLocalStorage/src/App.jsx
+++ 10todoContextLocalStorage/src/App.jsx
@@ -26,10 +26,13 @@ function App() {
     const todos = JSON.parse(localStorage.getItem("todos"))
 
     if (todos && todos.length > 0 ) {
-
+      setTodos(todos)
     }
   }, []) 
   
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
 
   return (
     <TodoProvider value = {{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
